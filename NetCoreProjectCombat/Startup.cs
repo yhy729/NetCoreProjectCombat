c2f11a022eedb05ffc82c701bc04d0909572d196
yhy <15818510483@126.com>
2019-12-23 22:00:13 +0800

    添加备注

--- NetCoreProjectCombat/Startup.cs
+++ NetCoreProjectCombat/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace NetCoreProjectCombat
 {
+    //Core 2.0
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
